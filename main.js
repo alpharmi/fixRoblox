34e2b8003745490765e0e0ecac792405d0d3dae9
alpha <Gianni.s177@outlook.com>
2022-03-22 13:29:07 +1100

    szrdffxtgcyhuj

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("test")
+var reorderHomepage = setInterval(function() {
+    var gameContainers = document.getElementsByClassName("game-home-page-container")[0]
+    if (gameContainers != undefined && gameContainers.children.length > 3) {
+        gameContainers = document.getElementsByClassName("game-home-page-container")[0].children
+
+        gameContainers[0].style.visibility = "hidden"
+        gameContainers[0].style.position = "absolute"
+        gameContainers[1].style.visibility = "hidden"
+        gameContainers[1].style.position = "absolute"
+
+        clearInterval(reorderHomepage)
+    }
+}, 100)
